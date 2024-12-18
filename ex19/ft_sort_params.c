8408080850e6a90185b389b01bb6d3adbde84800
Mehdi Rharouity <mrharoui@cbr10s6.42barcelona.com>
2024-12-18 18:40:39 +0100

    add ft_param

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -4,46 +4,54 @@
 
 void    ft_swap_argv(char **a, char **b);
 void    ft_output_argv(char **argv, int argc);
+void    ft_sort_param(int argc, char *argv[], int i, int x, int y, int flag);
 
 int main(int argc, char *argv[])
 {
     int i;
-    int y;
     int x;
+    int y;
     int flag;
 
     i = 0;
+    x = 0;
     y = 1;
-    x = 1;
     flag = 1;
+    if (argc < 2)
+    {
+        write(1, "\n", 1);
+        return (1);
+    }
+    ft_sort_param(argc, argv, i, x, y, flag);
+}
+
+void    ft_sort_param(int argc, char *argv[], int i, int x, int y, int flag)
+{
     while (flag == 1)
     {
         flag = 0;
+        y = 1;
+        x = 0;
         while (x < argc - 1)
         {
-            x += 1;
+            x = y + 1;
+            i = 0;
             while (argv[y][i] && argv[x][i])
             {
-                if (argv[y][i] < argv[x][i])
+                if (argv[y][i] > argv[x][i])
                 {
-
                     ft_swap_argv(&argv[y], &argv[x]);
-                    flag++;
-                    break;
+                    flag = 1;
                 }
+                if (argv[y][i] < argv[x][i])       
+                    break;
                 i++;
             }
-            i = 0;
-        }  
+            y++;
+        }
     }
-    printf("%s\n", argv[1]);
-    printf("%s\n", argv[2]);
-    printf("%s\n", argv[3]);
-    printf("%s\n", argv[4]);
-    exit(0);
     ft_output_argv(argv, argc);
 }
-  
 
 void    ft_swap_argv(char **a, char **b)
 {
