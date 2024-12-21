80e450a91cb36416da0ff6e77cf2b26ccab6b78a
medex24-bc <medex24@proton.me>
2024-12-21 10:41:17 +0100

    Update ft_sort_params.c

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,15 +6,22 @@
 /*   By: mrharoui <mrharoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 13:03:00 by mrharoui          #+#    #+#             */
-/*   Updated: 2024/12/19 13:44:02 by mrharoui         ###   ########.fr       */
+/*   Updated: 2024/12/21 10:38:07 by mrharoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 void	ft_swap_argv(char **a, char **b);
 void	ft_output_argv(char **argv, int argc);
 int		ft_sort_param(int argc, char *argv[], int x, int y);
+int		ft_strlen(char *str);
 
 int	main(int argc, char *argv[])
 {
@@ -26,9 +33,13 @@ int	main(int argc, char *argv[])
 	y = 1;
 	flag = 1;
 	if (argc < 2)
-	{
-		write(1, "\n", 1);
 		return (1);
+	if (argc == 2)
+	{
+		while (argv[y][x])
+			ft_putchar(argv[y][x++]);
+		ft_putchar('\n');
+		return (0);
 	}
 	while (flag == 1)
 	{
@@ -51,7 +62,8 @@ int	ft_sort_param(int argc, char *argv[], int x, int y)
 		i = 0;
 		while (argv[y][i] && argv[x][i])
 		{
-			if (argv[y][i] > argv[x][i])
+			if (argv[y][i] > argv[x][i] || (argv[y][i] == argv[x][i]
+				&& argv[y][i] > argv[x][i + 1]))
 			{
 				ft_swap_argv(&argv[y], &argv[x]);
 				return (1);
@@ -86,10 +98,20 @@ void	ft_output_argv(char **argv, int argc)
 		i = 0;
 		while (argv[x][i])
 		{
-			write(1, &argv[x][i], 1);
+			ft_putchar(argv[x][i]);
 			i++;
 		}
-		write(1, "\n", 1);
+		ft_putchar('\n');
 		x++;
 	}
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
